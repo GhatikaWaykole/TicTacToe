44c997a32e8936795b7240b45664244393bdb26a
ghatikawaykole <ghatika99@gmail.com>
2021-09-12 20:43:53 +0530

    Welcome to the tic tac toe

---
+++ src/com/example/TicTacToe/TicTacToe.java
@@ -0,0 +1,10 @@
+package com.example.TicTacToe;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String args[]) {
+        System.out.println("Welcome to the tic tac toe");
+    }
+
+}
