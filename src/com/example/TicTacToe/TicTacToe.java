2d4378c4fcd859d05e5471a7dd152a9fe5b76eba
ghatikawaykole <ghatika99@gmail.com>
2021-09-12 20:48:48 +0530

    Adding the char array to create a blank game board

--- src/com/example/TicTacToe/TicTacToe.java
+++ src/com/example/TicTacToe/TicTacToe.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String args[]) {
         System.out.println("Welcome to the tic tac toe");
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+        for(char[] row : gameBoard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
     }
 
 }
